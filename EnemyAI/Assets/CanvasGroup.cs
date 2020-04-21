f2bc4bd22f4f486aa85f85d0d1fc40eef497d59d
Jacob <jacobklukas@aol.com>
2020-04-21 13:57:19 -0400

    Alpha temp and object pickup UI
    contains the basic temperature and pickup UI, can be merged with master for the alpha

---
+++ EnemyAI/Assets/CanvasGroup.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasGroup : MonoBehaviour
+{
+
+    public GameObject Fbutton;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Fbutton.SetActive(false);
+        }
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            Fbutton.SetActive(true);
+        }
+    }
+}
---
+++ EnemyAI/Assets/TempControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TempControl : MonoBehaviour
+{
+
+    private float temp = 30f;
+    public Image tempUI;
+    private int timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tempUI.color = Color.Lerp(Color.blue, Color.red, temp/30);
+        timer++;
+        Debug.Log(timer);
+        if (timer % 12 == 0)
+        {
+            
+            temp--;
+        }
+    }
+}
