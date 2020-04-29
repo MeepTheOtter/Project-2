0eea770e3e6b24181f8886fd7dd1755e9be22c44
Shane Thornton <thornts7@ferris.edu>
2020-04-28 20:19:46 -0400

    AnimationsReIncluded

---
+++ EnemyAI/Assets/Scripts/AnimationScripts/RightHandMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightHandMovement : MonoBehaviour
+{
+    Vector3 startLocation;
+    float YMULT = .05f;
+    float XMULT = .05f;
+
+    float Speed = 7;
+    void Start()
+    {
+        startLocation = transform.localPosition;
+    }
+    void Update()
+    {
+        if (Input.GetAxis("Vertical") == 0 && Input.GetAxis("Horizontal") == 0)
+        {
+            Speed = 1;
+        } else
+        {
+            Speed = 7;
+        }
+        float time = (Time.time) * Speed;
+        float offset = Mathf.Sin(time);
+
+        Vector3 finalLocation = startLocation;
+        finalLocation += new Vector3(offset * XMULT, offset * YMULT, 0);
+        transform.localPosition = finalLocation;
+    }
+}
---
+++ EnemyAI/Assets/Scripts/AnimationScripts/SwayScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwayScript : MonoBehaviour
+{
+    Quaternion startRotation;
+    float Limiter = .05f;
+
+    void Start()
+    {
+        startRotation = transform.localRotation;
+    }
+    
+    void Update() 
+    {
+        float time = Time.time;
+        float RotateOffset = Mathf.Sin(time * 7);
+        bool Moving = (Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0) ? true : false;
+        if (Moving)
+        {
+            Quaternion A = startRotation;
+            startRotation *= Quaternion.Euler(0, 0, RotateOffset * Limiter);
+            transform.localRotation = A;
+        } else
+        {
+            transform.localRotation = startRotation;
+        }
+    }
+}
