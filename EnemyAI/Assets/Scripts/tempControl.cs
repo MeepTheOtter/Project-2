a946b6221634baff75fbaea003eb772cbb7ff1cd
Jacob <jacobklukas@aol.com>
2020-04-29 20:28:19 -0400

    added fire and minor UI changes
    add the fire particle system and added UI features

---
+++ EnemyAI/Assets/Scripts/tempControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class tempControl : MonoBehaviour
+{
+    private float temp = 30;
+    public RawImage tempUI;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tempUI.color = Color.Lerp(Color.blue, Color.red, temp / 30);
+        temp--;
+    }
+}
